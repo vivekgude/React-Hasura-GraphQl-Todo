85ea06d7861336270ee00b8ce9d5626bdbd5764e
Vivek Gude <59313903+vivekgude@users.noreply.github.com>
2022-04-06 02:28:11 +0530

    make better

--- src/components/Auth/auth0-variables.js
+++ src/components/Auth/auth0-variables.js
@@ -1,5 +1,5 @@
 export const AUTH_CONFIG={
 domain: 'dev-451mlaqa.us.auth0.com',
 clientId: '8zOpDK4zQtqpir3NzwRqfkWyjWFLrTkA',
-callbackUrl: 'http://localhost:3000/callback'
+callbackUrl: 'http://react-hasura-graphql-todo.netlify.app/callback'
 };
